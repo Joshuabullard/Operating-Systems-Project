0f1646ce39af91f7cfff3f73fa45e793eba850e3
bullardj171 <joshbullard2437@gmail.com>
2020-02-17 12:44:03 -0500

    Added methods to RoundRobin class

--- scheduler-master/src/scheduler/RoundRobin.java
+++ scheduler-master/src/scheduler/RoundRobin.java
@@ -1,7 +1,45 @@
 package scheduler;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-/**
- * Created by bullardj171 on 2/17/20.
- */
-public class RoundRobin {
+
+public class RoundRobin extends Scheduler {
+
+
+    private LinkedList<ProcessControlBlock> readyQueue;
+    private LinkedList<ProcessControlBlock> waitQueue;
+    private LinkedList<ProcessControlBlock> terminated;
+
+    //default constructor
+    public RoundRobin(int contextSwitchTime) {
+        super(contextSwitchTime);
+        readyQueue = new LinkedList<ProcessControlBlock>();
+        waitQueue = new LinkedList<ProcessControlBlock>();
+        terminated = new LinkedList<ProcessControlBlock>();
+    }
+
+    @Override
+    public void add(ProcessControlBlock pcb) {
+
+    }
+
+    @Override
+    public ProcessControlBlock next() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void execute(ProcessControlBlock pcb) {
+
+    }
+
+    @Override
+    public Iterator<ProcessControlBlock> iterator() {
+        return null;
+    }
 }
