d6cd11c09ac8ba94928d050244edaf29b2c90f12
Joshuabullard <46793845+Joshuabullard@users.noreply.github.com>
2020-02-26 15:41:53 -0500

    Added ioRequestTime

--- scheduler-master/src/scheduler/ProcessControlBlock.java
+++ scheduler-master/src/scheduler/ProcessControlBlock.java
@@ -48,7 +48,7 @@ public class ProcessControlBlock {
             duration -= currentBurstDuration;
             currentBurstDuration = ioBurstTime;
             state = WAITING;
-            ioRequestTime = clock;
+            ioRequestTime = clock + usedTime - 1;
             System.out.println(pid + " remaining duration: " + duration);
             return usedTime;
         } else {
@@ -86,6 +86,6 @@ public class ProcessControlBlock {
 
     public String toString() {
         return "{" + pid + ", " + state + ", " + duration + ", " + cpuBurstTime +
-                    ", " + ioBurstTime + ", " + currentBurstDuration + "}";
+                ", " + ioBurstTime + ", " + currentBurstDuration + "}";
     }
 }
